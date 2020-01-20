6ebd68ef6a85b3b4f66ff31ec0e37d186d934563
Inayaili de León <inayaili@gmail.com>
2020-01-20 16:43:17 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Doctocat Template',
-    shortName: 'Template',
-    description: 'My site description',
+    title: 'Doctocasts Trial',
+    shortName: 'Doctocasts',
+    description: 'A site to test the Doctocat template',
   },
   plugins: ['@primer/gatsby-theme-doctocat'],
 }
